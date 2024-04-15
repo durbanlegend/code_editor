21ff68e957d6529630da230d323cb4f79263adf8
durbanlegend <dforbes@iafrica.com>
2024-04-15 21:16:52 +0200

    Add persistence

--- src/app.rs
+++ src/app.rs
@@ -33,9 +33,9 @@ impl TemplateApp {
 
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
-        // if let Some(storage) = cc.storage {
-        //     return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        // }
+        if let Some(storage) = cc.storage {
+            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        }
 
         TemplateApp::default()
     }
