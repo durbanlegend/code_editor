a0abb9b8839597c3b77b8ca42c496e4ea30cedc4
durbanlegend <dforbes@iafrica.com>
2024-04-15 21:31:03 +0200

    Update app.rs

--- src/app.rs
+++ src/app.rs
@@ -2,10 +2,6 @@
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
-    // label: String,
-    // #[serde(skip)] // This how you opt-out of serialization of a field
-    // value: f32,
     language: String,
     code: String,
 }
@@ -42,7 +38,7 @@ impl TemplateApp {
 }
 
 impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
+    /// Called by the framework to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
@@ -72,42 +68,7 @@ impl eframe::App for TemplateApp {
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // // The central panel the region left after adding TopPanel's and SidePanel's
-            // ui.heading("Code editor");
-
-            // ui.horizontal(|ui| {
-            //     ui.label("Write something: ");
-            //     ui.text_edit_singleline(&mut self.label);
-            // });
-
-            // ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            // if ui.button("Increment").clicked() {
-            //     self.value += 1.0;
-            // }
-
-            // ui.separator();
-
-            // ui.add(egui::github_link_file!(
-            //     "https://github.com/emilk/eframe_template/blob/master/",
-            //     "Source code."
-            // ));
-
-            // ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-            //     powered_by_egui_and_eframe(ui);
-            //     egui::warn_if_debug_build(ui);
-            // });
             let Self { language, code } = self;
-            //             let code = r#"// A very simple example
-            // fn main() {
-            // 	println!("Hello world!");
-            // }
-            // "#;
-
-            ui.horizontal(|ui| {
-                ui.set_height(0.0);
-                ui.label("An example of syntax highlighting in a TextEdit.");
-                // ui.add(crate::egui_github_link_file!());
-            });
 
             if cfg!(feature = "syntect") {
                 ui.horizontal(|ui| {
@@ -160,17 +121,3 @@ impl eframe::App for TemplateApp {
         });
     }
 }
-
-// fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-//     ui.horizontal(|ui| {
-//         ui.spacing_mut().item_spacing.x = 0.0;
-//         ui.label("Powered by ");
-//         ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-//         ui.label(" and ");
-//         ui.hyperlink_to(
-//             "eframe",
-//             "https://github.com/emilk/egui/tree/master/crates/eframe",
-//         );
-//         ui.label(".");
-//     });
-// }
